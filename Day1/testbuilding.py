3d8549eba334f845f257c334415aa7e625c0c7ee
VasiliySilver <s555133@mail.ru>
2020-04-06 21:33:14 +0300

    Добавил файл тест для тестовых случаев

---
+++ Day1/testbuilding.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestBuilding(unittest.TestCase):
+    def test_bu(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
